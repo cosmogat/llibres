13b933a3efeaa94be038d4adbec886777badfd7b
cosmogat <cosmogatespacial@gmail.com>
2018-08-28 11:40:29 +0200

    book adding template

--- llocs/modllibres.php
+++ llocs/modllibres.php
@@ -15,9 +15,21 @@ class LlocModllibres {
     public $alert = 0;
     public $autor = array();
     
-    public function calculs() { }
+    public function calculs() {
+        $this->mod = intval(Peticio::obte("mode"));
+        if (!preg_match("/^[0-2]$/", $this->mod))
+            redireccionar(Link::url("index"));
+    }
 
     public function imprimir() {
+        $tpl = new Plantilla("./html");
+        if ($this->mod == 0) {
+            $tpl->carregar("modllibres");
+            $tpl->mostrar("form_afegir");
+            $tpl->set("ACTION", Link::url("afegir-llibres"));
+            $tpl->set("TORNAR", Link::url("index")); // canviar a menu de configuració
+            $tpl->imprimir();
+        }
         Peticio::impr();
     }
 

0190291576fdf26b6e9605e7654d3fb22c63e245
cosmogat <cosmogatespacial@gmail.com>
2018-08-30 19:16:29 +0200

    add book object

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ require_once("./inc/ses.obj.php");
 require_once("./inc/usu.obj.php");
 require_once("./inc/lnk.obj.php");
 require_once("./inc/con.obj.php");
+require_once("./inc/lli.obj.php");
 require_once("./inc/fun.inc.php");
 require_once("./inc/fnc.inc.php");
 require_once("./cnf/conf_" . quinPerfil() . ".inc.php");
